6e834f2424c1891d3ff06edaa8be3212d9b78a19
Alissa <32532106+qiualissa@users.noreply.github.com>
2021-11-05 17:12:44 -0400

    cleaning

--- src/Square.py
+++ src/Square.py
@@ -3,8 +3,10 @@ class Square:
         self.value = value
         # if true the number is possible
         # example self.binCondition[1] = True
-            # the number 2 is possible for this square
-        if value == 0 :
-            self.binCondition = [True,True,True,True,True,True,True,True,True]
+        # the number 2 is possible for this square
+        if value == 0:
+            self.binCondition = [
+                True, True, True, True, True, True, True, True, True]
         else:
-            self.binCondition = [False,False,False,False,False,False,False,False,False]
+            self.binCondition = [
+                False, False, False, False, False, False, False, False, False]
