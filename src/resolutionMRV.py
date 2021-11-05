4ecf2299cd224655c4b6701a6df99aca334f5cca
Alissa <32532106+qiualissa@users.noreply.github.com>
2021-11-05 17:29:21 -0400

    cleaning

--- src/resolutionMRV.py
+++ src/resolutionMRV.py
@@ -1,70 +1,77 @@
 import SudokuGrid
 import numpy as np
 
-#Fonction de backtracking idiot
-def backtracking(grid, nbIte = 1):
-    #Cherche le prochain vide:
-    r,c = NextEmpty(grid)
-    if r == -1: #Le sudoku est terminé
+
+# Fonction de backtracking idiot
+def backtracking(grid, nbIte=1):
+    # Cherche le prochain vide:
+    r, c = NextEmpty(grid)
+    if r == -1:     # Le sudoku est terminé
         print(nbIte)
         return True
     else:
-        for val in range(1,10):
+        for val in range(1, 10):
             if isPossible(grid, r, c, val):
                 grid[r][c].value = val
-                
+
                 if backtracking(grid, nbIte + 1):
                     return True
                 grid[r][c].value = 0
-        return False         
+        return False
 
-def backtrackingMRV(grid, nbIte = 1):
-    #Cherche le prochain vide:
-    r,c = NextEmptyMRV(grid)
-    if r == -1: #Le sudoku est terminé
+
+def backtrackingMRV(grid, nbIte=1):
+    # Cherche le prochain vide:
+    r, c = NextEmptyMRV(grid)
+    if r == -1:     # Le sudoku est terminé
         print(nbIte)
         return True
     else:
-        for val in range(1,10):
+        for val in range(1, 10):
             if isPossible(grid, r, c, val):
                 grid[r][c].value = val
-                
+
                 if backtrackingMRV(grid, nbIte + 1):
                     return True
                 grid[r][c].value = 0
-        
-        return False         
+
+        return False
 
 
 def NextEmptyMRV(grid):
     gridPoss = gridNumPossibility(grid)
-    if gridPoss.any() != np.zeros((9,9)).any():
+    if gridPoss.any() != np.zeros((9, 9)).any():
         min = np.min(gridPoss[np.nonzero(gridPoss)])
         for row in range(9):
             for col in range(9):
-                if  gridPoss[row, col] == min:
+                if gridPoss[row, col] == min:
                     return row, col
-    
-    return NextEmpty(grid) # Si il y a encore des cases vides mais sans solution
-    
-def gridNumPossibility(grid): #Retourne le nombre de possibilité (en valeur) pour chaque case
-    gridNum = np.zeros((9,9))
+    # Si il y a encore des cases vides mais sans solution
+    return NextEmpty(grid)
+
+
+# Retourne le nombre de possibilité (en valeur) pour chaque case
+def gridNumPossibility(grid):
+    gridNum = np.zeros((9, 9))
     for row in range(9):
         for col in range(9):
             if grid[row][col].value == 0:
-                for val in range(1,10):
+                for val in range(1, 10):
                     if isPossible(grid, row, col, val):
                         gridNum[row][col] += 1
     return gridNum
+
 
 def NextEmpty(grid):
     for row in range(9):
         for col in range(9):
             if grid[row][col].value == 0:
                 return row, col
-    return -1,-1    
+    return -1, -1
+
+
 def isPossible(grid, row, col, val):
-    
+
     # Meme num sur la ligne
     for x in range(9):
         if grid[row][x].value == val:
@@ -74,7 +81,7 @@ def isPossible(grid, row, col, val):
     for x in range(9):
         if grid[x][col].value == val:
             return False
-    
+
     # Meme num sur la sous matrice
     subRow = row - row % 3
     subCol = col - col % 3
@@ -82,10 +89,9 @@ def isPossible(grid, row, col, val):
         for j in range(3):
             if grid[i + subRow][j + subCol].value == val:
                 return False
-    
+
     # Le chiffre est possible
     return True
-
 
 
 sudo1 = SudokuGrid.SudokuGrid(9)
